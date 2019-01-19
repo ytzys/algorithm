2e67f0e0844c7fbec6654654467d180c830eef26
ytzys <ytzys@163.com>
2019-01-20 00:05:40 +0800

    19.1.20 0:05

---
+++ com/ytzys/algorithm/leedcode/RemoveElement.java
@@ -0,0 +1,28 @@
+package com.ytzys.algorithm.leedcode;
+
+import org.junit.Test;
+
+/**
+ * Created by ZhangYisu on 2019/1/19.
+ */
+public class RemoveElement {
+    @Test
+    public void test() {
+        int[] arr = new int[]{2, 2, 1};
+        System.out.println(removeElement(arr, 2));
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public int removeElement(int[] nums, int val) {
+        int curIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[curIndex] = nums[i];
+                curIndex++;
+            }
+        }
+        return curIndex;
+    }
+}
---
+++ com/ytzys/algorithm/leedcode/StrStr28.java
@@ -0,0 +1,35 @@
+package com.ytzys.algorithm.leedcode;
+
+import org.junit.Test;
+
+/**
+ * Created by ZhangYisu on 2019/1/19.
+ */
+public class StrStr28 {
+    @Test
+    public void test() {
+        System.out.println(strStr("134423", "44"));
+    }
+
+    public int strStr(String haystack, String needle) {
+        if (needle == null || needle.equals("")) {
+            return 0;
+        }
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            int k = i;
+            for (; j < needle.length(); j++, k++) {
+                if (haystack.charAt(k) != needle.charAt(j)) {
+                    break;
+                }
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
