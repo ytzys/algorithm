22c8651c0d2214dc306f4693995eee188ffa8201
ytzys <ytzys@163.com>
2019-01-21 00:13:15 +0800

    19.1.21 0:15

---
+++ com/ytzys/algorithm/leedcode/CountAndSay.java
@@ -0,0 +1,46 @@
+package com.ytzys.algorithm.leedcode;
+
+import org.junit.Test;
+
+/**
+ * Created by ZhangYisu on 2019/1/20.
+ */
+public class CountAndSay {
+    @Test
+    public void test() {
+        System.out.println(countAndSay(3));
+    }
+
+    /**
+     * TODO waiting for optimising
+     *
+     * @param n
+     * @return
+     */
+    public String countAndSay(int n) {
+        String[] result = new String[n];
+        result[0] = "1";
+        for (int i = 1; i < result.length; i++) {
+            String target = result[i - 1];
+            char curCar = target.charAt(0);
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < target.length(); j++) {
+                if (target.charAt(j) == curCar) {
+                    count++;
+                } else {
+                    sb.append(count).append(curCar);
+                    curCar = target.charAt(j);
+                    count = 1;
+                }
+            }
+            sb.append(count).append(curCar);
+            result[i] = sb.toString();
+        }
+//        for (String tmp : result) {
+//            System.out.println(tmp);
+//        }
+        return result[n - 1];
+    }
+
+}
---
+++ com/ytzys/algorithm/leedcode/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+package com.ytzys.algorithm.leedcode;
+
+import org.junit.Test;
+
+/**
+ * Created by ZhangYisu on 2019/1/20.
+ */
+public class SearchInsertPosition {
+    @Test
+    public void test() {
+        System.out.println(searchInsert(new int[]{1, 2, 3, 5}, 6));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int index = 0;
+        for (; index < nums.length; index++) {
+            if (nums[index] >= target) {
+                return index;
+            }
+        }
+        return index;
+    }
+}
