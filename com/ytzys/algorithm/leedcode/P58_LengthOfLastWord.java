dd87dbdcfe516d90186e5b38846b86b30af921ed
ytzys <ytzys@163.com>
2019-01-22 00:14:51 +0800

    19.1.22 0:15

--- com/ytzys/algorithm/leedcode/P58_LengthOfLastWord.java
+++ com/ytzys/algorithm/leedcode/P58_LengthOfLastWord.java
@@ -8,7 +8,45 @@ import org.junit.Test;
 public class P58_LengthOfLastWord {
     @Test
     public void test() {
-        System.out.println(lengthOfLastWord("hello worlxd "));
+        System.out.println(lengthOfLastWord1("hello worlxd "));
+    }
+
+    public int lengthOfLastWord2(String s) {
+        int lastNoneSpaceIndex = -1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ' && lastNoneSpaceIndex == -1) {
+                lastNoneSpaceIndex = i;
+            } else if (s.charAt(i) == ' ' && lastNoneSpaceIndex != -1)
+                return lastNoneSpaceIndex - i;
+        }
+        if (lastNoneSpaceIndex == -1)
+            return 0;
+        else
+            return lastNoneSpaceIndex + 1;
+    }
+
+    /**
+     * This method is slower than lengthOfLastWord! Can you beleive that?
+     * I think it's because in the method lengthOfLastWord, there are two comparations
+     * at most in every loop, but in this method, there are four. So we can say that,
+     * so called opitmization doesn't work all the time.
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLastWord1(String s) {
+        int lastNoneSpaceIndex = -1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ' && lastNoneSpaceIndex == -1) {
+                lastNoneSpaceIndex = i;
+            }
+            if (s.charAt(i) == ' ' && lastNoneSpaceIndex != -1)
+                return lastNoneSpaceIndex - i;
+        }
+        if (lastNoneSpaceIndex == -1)
+            return 0;
+        else
+            return lastNoneSpaceIndex + 1;
     }
 
     public int lengthOfLastWord(String s) {
